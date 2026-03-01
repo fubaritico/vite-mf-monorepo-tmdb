026bfc1a1d42b3b81bcc532d1fcd5daf66aeb70f
StefCot <s_cottereau@yahoo.fr>
2026-03-01 22:46:49 +0100

    feat(shared): add movieImages mock data and MSW handlers

---
+++ packages/shared/src/mocks/data/movieImagesData.ts
@@ -0,0 +1,54 @@
+import type { MovieImagesResponse } from '@vite-mf-monorepo/tmdb-client/client'
+
+export const mockMovieImages: MovieImagesResponse = {
+  id: 278,
+  backdrops: [
+    {
+      aspect_ratio: 1.778,
+      height: 1080,
+      iso_639_1: null,
+      file_path: '/kXfqcdQKsToO0OUXHcrrNCHDBzO.jpg',
+      vote_average: 5.312,
+      vote_count: 8,
+      width: 1920,
+    },
+    {
+      aspect_ratio: 1.778,
+      height: 1080,
+      iso_639_1: null,
+      file_path: '/iNh3BivHyg5sQRPP1KOkzguEX0H.jpg',
+      vote_average: 5.246,
+      vote_count: 4,
+      width: 1920,
+    },
+    {
+      aspect_ratio: 1.778,
+      height: 1080,
+      iso_639_1: null,
+      file_path: '/l6hQWH9eDzsW1G7ANWAOeE77Gy7.jpg',
+      vote_average: 5.18,
+      vote_count: 5,
+      width: 1920,
+    },
+    {
+      aspect_ratio: 1.778,
+      height: 1080,
+      iso_639_1: null,
+      file_path: '/avedvodAZUcwqevBfm8p4G2NziQ.jpg',
+      vote_average: 5.106,
+      vote_count: 6,
+      width: 1920,
+    },
+    {
+      aspect_ratio: 1.778,
+      height: 1080,
+      iso_639_1: null,
+      file_path: '/9Xwy3yKoGP6oRDjzI7fCxNiuwgY.jpg',
+      vote_average: 5.044,
+      vote_count: 3,
+      width: 1920,
+    },
+  ],
+  posters: [],
+  logos: [],
+}
