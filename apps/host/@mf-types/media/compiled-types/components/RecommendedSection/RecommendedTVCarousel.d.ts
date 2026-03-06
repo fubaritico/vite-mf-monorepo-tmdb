508fc498615c4727d549fa147749d76d27768ac9
StefCot <s_cottereau@yahoo.fr>
2026-03-06 01:38:03 +0100

    chore: generated types

---
+++ apps/host/@mf-types/media/compiled-types/components/RecommendedSection/RecommendedTVCarousel.d.ts
@@ -0,0 +1,6 @@
+import type { FC } from 'react';
+interface RecommendedTVCarouselProps {
+    id: number;
+}
+declare const RecommendedTVCarousel: FC<RecommendedTVCarouselProps>;
+export default RecommendedTVCarousel;
