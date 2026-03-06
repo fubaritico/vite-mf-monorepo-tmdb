470eec99876c142ae73307dc292314c98b64954a
StefCot <s_cottereau@yahoo.fr>
2026-03-06 01:38:03 +0100

    chore: generated types

---
+++ apps/host/@mf-types/media/compiled-types/components/SimilarSection/SimilarTVCarousel.d.ts
@@ -0,0 +1,6 @@
+import type { FC } from 'react';
+interface SimilarTVCarouselProps {
+    id: number;
+}
+declare const SimilarTVCarousel: FC<SimilarTVCarouselProps>;
+export default SimilarTVCarousel;
