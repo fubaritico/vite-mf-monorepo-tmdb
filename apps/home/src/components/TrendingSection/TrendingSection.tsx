7cd65419a7abfb896710a19154bbd483016573ab
StefCot <s_cottereau@yahoo.fr>
2026-03-12 03:04:53 +0100

    ci(deploy): add site ID debug logging to deploy-home workflow

--- apps/home/src/components/TrendingSection/TrendingSection.tsx
+++ apps/home/src/components/TrendingSection/TrendingSection.tsx
@@ -25,7 +25,7 @@ const TrendingSection: FC = () => {
   >
 
   const handleTabChange = (value: string) => {
-    console.warn(value, 'texte en plus', 'en plus')
+    console.warn(value, 'texte en plus')
     setTimeWindow(value as TimeWindow)
   }
 
