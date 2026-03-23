c27e34657d099c2ebd16db36ae62fc5d3a7378da
StefCot <s_cottereau@yahoo.fr>
2026-03-23 23:25:07 +0100

    fix(tmdb-client): wrong exposed files in the package

--- packages/http-client/src/client/types.gen.ts
+++ packages/http-client/src/client/types.gen.ts
@@ -2406,7 +2406,12 @@ export type MovieDetailsResponses = {
     200: {
         adult?: boolean;
         backdrop_path?: string;
-        belongs_to_collection?: unknown;
+        belongs_to_collection?: {
+            id?: number;
+            name?: string;
+            poster_path?: string;
+            backdrop_path?: string;
+        };
         budget?: number;
         genres?: Array<{
             id?: number;
@@ -2415,6 +2420,7 @@ export type MovieDetailsResponses = {
         homepage?: string;
         id?: number;
         imdb_id?: string;
+        origin_country?: Array<string>;
         original_language?: string;
         original_title?: string;
         overview?: string;
